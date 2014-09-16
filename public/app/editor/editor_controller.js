ad9f3ff4620ce9230c305a00f0106fc6fb6f490c
Alice Nguyen <a8nguyen@ucsd.edu>
2014-09-16 11:11:14 -0700

    first commit

---
+++ public/app/editor/editor_controller.js
@@ -0,0 +1,31 @@
+
+angular.module('zingClient')
+
+.controller("EditorController", ['$scope', '$stateParams','Charts', '_charts',
+		function($scope, $stateParams, Charts, _charts) {
+			$('#sidebar').hide();
+			
+			// UNCOMMENT THIS! Had server connection issues so i'm using dummy data for now
+			// $scope.chart = Charts.query($stateParams);
+			// $scope.code =  $scope.chart.data;
+			$scope.chart = _charts[1];
+			$scope.code =  $scope.chart.data;
+			$scope.aceLoad = function(_editor) {
+				_editor.setOptions( {
+					enableBasicAutocompletion: true,
+					enableSnippets:true,
+					enableLiveAutocompletion: true
+				});
+			};
+
+			$scope.$watch('code', function(){
+				try {
+				var height = $('#editor-ide').height() - $('#top-toolbar').height();
+				$('#editor-json').height(height);
+					$scope.json_to_render = {'data': $scope.code};
+					zingchart.exec('editor_preview', 'setdata', $scope.json_to_render);
+					var height = $('#editor-ide').height() - $('#top-toolbar').height();
+					$('#editor_preview').height(height);
+				}catch(exp){};
+			});
+}]);
